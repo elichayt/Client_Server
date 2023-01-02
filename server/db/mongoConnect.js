604fd03eeca50ab4453105689dab2d3fa0f171fb
elichayt <116493332+elichayt@users.noreply.github.com>
2023-01-02 09:43:50 +0000

    test

--- server/db/mongoConnect.js
+++ server/db/mongoConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose'); //connect to mongoose module
 require('dotenv').config()
 
-main().catch(err => console.log(err)); //Error Message if not working the connect
+main().catch(err => console.log(err)); //Error Message if not working the connectt
 
 async function main() { //connect Data Base from Mongo Atlas by user & password 
     await mongoose.connect(`mongodb://127.0.0.1:27018/myapp`); //(feb22 == name DataBase)
